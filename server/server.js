84c3d0a0162729fb4dd7b587b6fd028ed0142d48
Ragibnoor <ragibnoor8766@gmail.com>
2026-08-16 17:20:42 +0530

    Seetup Node/Express backend with mysql connection

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const db = require("./config/db");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Student Utility Hub API is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
